53b7efbfecc6b4bb886618b327c5788bb17c1d25
phung12017 <tranthanhphung092015@gmail.com>
2021-03-13 02:49:06 +0700

    /login

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/',(req,res)=>{
     res.json({msg:'xxxx'})
 })
 
+app.get('/login',(req,res)=>{
+    res.json({msg:'login'})
+})
 
 //open server at port 3000:
 app.listen(process.env.PORT || 3000, () => {
