e720f233b05badf86af128990886fdfd81585990
stomper013 <stomper013@gmail.com>
2021-03-19 09:07:47 +0700

    Update
    Add Order , Cart
    Update Cart

--- models/order.js
+++ models/order.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const childSchema = new Schema({
-    _idProduct: { type: Schema.Types.ObjectId, ref: 'products' }
+    _idProduct: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'products',
+        require: true,
+    },
+    quality: {
+        type: Number,
+        min: [1, 'Quantity can not be less then 1.'],
+        require: true,
+    }
 });
 
 const orderSchema = new Schema({
-    _uid: { type: Schema.Types.ObjectId },
+    _uid: { 
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        require: true
+    },
     products: [childSchema],
     createAt: { type: Date },
     updateAt: { type: Date },
